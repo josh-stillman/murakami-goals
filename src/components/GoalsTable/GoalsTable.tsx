51bbf796641540181bfb9f1bef1762324be542fb
Josh Stillman <joshstillman@gmail.com>
2025-01-10 16:48:43 -0500

    fix scroll into view behavior

--- src/components/GoalsTable/GoalsTable.tsx
+++ src/components/GoalsTable/GoalsTable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import type { GoalDTO } from '../../db/types';
 import { addDaysToDate, getToday } from '../../db/utils';
 import { WeekGoal } from '../WeekGoal/WeekGoal';
@@ -11,6 +12,21 @@ interface Props {
 export const GoalsTable = ({ goalDTOs, currentMonday }: Props) => {
   const today = getToday();
 
+  const todayDiv = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      todayDiv.current?.scrollIntoView({
+        inline: 'center',
+        behavior: 'smooth',
+      });
+      console.log('scrolling!', todayDiv.current);
+    }, 500);
+  }, []);
+
+  const isToday = (dayOfWeek: number) =>
+    today.getTime() === addDaysToDate(currentMonday, dayOfWeek).getTime(); // monday = 0
+
   return (
     <div className={styles.goalTable}>
       <div className={styles.stickyColumn} />
@@ -18,7 +34,7 @@ export const GoalsTable = ({ goalDTOs, currentMonday }: Props) => {
       <div
         className={`
           ${styles.tableHeader}
-          ${today.getTime() === currentMonday.getTime() ? styles.todayHeader : ''}`}
+          ${isToday(0) ? styles.todayHeader : ''}`}
       >
         Mon
       </div>
@@ -26,12 +42,9 @@ export const GoalsTable = ({ goalDTOs, currentMonday }: Props) => {
       <div
         className={`
           ${styles.tableHeader}
-          ${
-            today.getTime() === addDaysToDate(currentMonday, 1).getTime()
-              ? styles.todayHeader
-              : ''
-          }
+          ${isToday(1) ? styles.todayHeader : ''}
         `}
+        ref={isToday(2 + 1) ? todayDiv : undefined}
       >
         Tue
       </div>
@@ -39,12 +52,9 @@ export const GoalsTable = ({ goalDTOs, currentMonday }: Props) => {
       <div
         className={`
           ${styles.tableHeader}
-          ${
-            today.getTime() === addDaysToDate(currentMonday, 2).getTime()
-              ? styles.todayHeader
-              : ''
-          }
+          ${isToday(2) ? styles.todayHeader : ''}
         `}
+        ref={isToday(2 + 2) ? todayDiv : undefined}
       >
         Wed
       </div>
@@ -52,12 +62,9 @@ export const GoalsTable = ({ goalDTOs, currentMonday }: Props) => {
       <div
         className={`
           ${styles.tableHeader}
-          ${
-            today.getTime() === addDaysToDate(currentMonday, 3).getTime()
-              ? styles.todayHeader
-              : ''
-          }
+          ${isToday(3) ? styles.todayHeader : ''}
         `}
+        ref={isToday(3 + 2) ? todayDiv : undefined}
       >
         Thurs
       </div>
@@ -65,12 +72,9 @@ export const GoalsTable = ({ goalDTOs, currentMonday }: Props) => {
       <div
         className={`
           ${styles.tableHeader}
-          ${
-            today.getTime() === addDaysToDate(currentMonday, 4).getTime()
-              ? styles.todayHeader
-              : ''
-          }
+          ${isToday(4) ? styles.todayHeader : ''}
         `}
+        ref={isToday(4 + 2) ? todayDiv : undefined}
       >
         Fri
       </div>
@@ -78,11 +82,7 @@ export const GoalsTable = ({ goalDTOs, currentMonday }: Props) => {
       <div
         className={`
           ${styles.tableHeader}
-          ${
-            today.getTime() === addDaysToDate(currentMonday, 5).getTime()
-              ? styles.todayHeader
-              : ''
-          }
+          ${isToday(5) ? styles.todayHeader : ''}
         `}
       >
         Sat
@@ -91,11 +91,7 @@ export const GoalsTable = ({ goalDTOs, currentMonday }: Props) => {
       <div
         className={`
           ${styles.tableHeader}
-          ${
-            today.getTime() === addDaysToDate(currentMonday, 6).getTime()
-              ? styles.todayHeader
-              : ''
-          }
+          ${isToday(6) ? styles.todayHeader : ''}
         `}
       >
         Sun
